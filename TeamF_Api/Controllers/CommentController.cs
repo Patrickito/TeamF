00e142d49fde7b5efa45a85e21bdcc5abe0358aa
Kelkó Balázs <kelkobalazs@gmail.com>
2021-11-24 16:36:59 +0100

    Add Comment controller

---
+++ TeamF_Api/Controllers/CommentController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamF_Api.DAL.Entity;
+using TeamF_Api.Security;
+using TeamF_Api.Services.Interfaces;
+
+namespace TeamF_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentService _service;
+
+        public CommentController(ICommentService service)
+        {
+            _service = service;
+        }
+
+        [Authorize]
+        [HttpGet("CaffFile/{id}")]
+        public async Task<ActionResult<List<Comment>>> GetCaffFileComment(int id)
+        {
+            return await _service.GetCommentsForCaff(User.Identity.Name, id);
+        }
+
+        [Authorize(Policy = SecurityConstants.AdminPolicy)]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _service.DeleteComment(User.Identity.Name, id);
+            return NoContent();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<Comment>> AddComment([FromBody] Comment newComment)
+        {
+            return await _service.AddComment(User.Identity.Name, newComment);
+        }
+
+    }
+}
--- TeamF_Api/Startup.cs
+++ TeamF_Api/Startup.cs
@@ -93,6 +93,7 @@ namespace TeamF_Api
 
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<ITokenGenerator, JwtTokenGenerator>();
+            services.AddScoped<ICommentService, CommentService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
