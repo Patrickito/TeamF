a2098e6d8a6a3adb32ccd31cb83ae4ff65060fed
RatPete <perger.peter.99@gmail.com>
2021-11-28 17:23:53 +0100

    userrole

--- compsecfrontend/compSecFrontEnd/src/app/admin-dashboard/admin-dashboard.component.ts
+++ compsecfrontend/compSecFrontEnd/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CaffEntity, RoleChangeDto, User } from '../api/models';
+import { CaffEntity, RoleChangeDto, User, UserDto } from '../api/models';
 import { AuthenticationService, CaffService } from '../api/services';
 
 @Component({
@@ -9,7 +9,7 @@ import { AuthenticationService, CaffService } from '../api/services';
 })
 export class AdminDashboardComponent implements OnInit {
   caffImgs:CaffEntity[]=[]
-  users:User[]=[]
+  users:UserDto[]=[]
   selectedUser:RoleChangeDto={
 
   }
@@ -18,18 +18,34 @@ export class AdminDashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.reloadCaffs()
+    this.reloadUsers()
   }
   changeUser(){
     this.authService.changeRoles({body:this.selectedUser})
   }
   RemoveCaff(e:any,img:CaffEntity){
     this.caffService.deleteCaffFile({id:img.id!!}).subscribe(_=>this.reloadCaffs())
-    
+  }
+  RemoveUser(e:any,user:UserDto){
+  }
+  reloadUsers(){
+    this.authService.getAllUsers().subscribe(_=>this.users=_)
   }
   reloadCaffs(){
     this.caffService.getAllCaffFiles().subscribe(_=>{this.caffImgs=_;console.log(_)})
   }
+  removeRole(e:any,user:UserDto,role:string){
+    let newRole:string[]=[]
+    for(let i=0;i<user.roles!!.length;i++){
+      if(user.roles!![i].toUpperCase()===role.toUpperCase()){
+        newRole[i]=user.roles!![i]
+      }
+    }
+    let roleDto:RoleChangeDto ={roles:newRole,userName:user.userName!!}
+    this.authService.changeRoles({body:roleDto}).subscribe()
+    
 
+  }
 
 
 }
